cf9f6de3324997196fcc9bb9b23011abaa877b56
Jalolov Laziz <103228220+LazizJalolov@users.noreply.github.com>
2022-04-13 01:38:31 +0500

    Create shaxsiyvamaxviy.py
    Kirma kuyasan

---
+++ shaxsiyvamaxviy.py
@@ -0,0 +1,15 @@
+def savol(ism):
+    print(f"Salom bu xabarlarni ko'rib turgan bo'lsang rahmat har bir savolga Ha yoki Yo'q deb javob ber {ism.title()}")
+savol(input("Ismingni yoz"))   
+
+javob =input("O'ylab ko'rib shu qarorga keldim Men bilan hozir shunchaki do'st bo'lishga nima deysan Javob yoz Ha/Yoʻq  Boshi katta harfda bo'lsin")
+if javob == "Ha":  
+    print(f"Ha degan bo'lsang yaxshi keyingi savollarga javob yoz")
+else:
+    print("Nega unday deb yozding dmayman shunchaki shu paytgacha seni xohishing bo'lib keldi bundan keyin ham shunday boʻladi mayli nma ham derdm ko'z ko'zga tushganda ko'rishguncha")
+a =input("Sen menga o'qishim kerak, gaplashishni  hali o'z voxti soati bor deding, men esa shunda ham seni tushunmabman buning uchun ham uzr, o'sha payti menga keling ikkalamiz ham birga o'qishga qattiq tayyorgarlik koʻramz vaqtimizni behuda sarflamasligimiz kere deb aytganingda men tushunardm hozir ham kech emas men bn birga o'qishga tayyorgarlik koʻrishga nma deysan Javob yoz Ha/Yo'q") 
+if a == "Ha":
+    print(f"Raxmat,    senga informatika va matematika fanlari kerak boʻlsa matematikani o'rganish menimcha ja uncha qiyinchilik tugʻdirmaydi sen dasturlashga qiziqsang Dastur yaratadigan odam birinchi oʻrinda o'z dasturlash tilini tanlab olishi kerak masalan shu dasturni Pythonda yozyapman hozir dunyodagi eng oson dasturlash tili Python bo'lishi kerak menimcha o'zing tanla Qaysi dasturlash tilini tanlasang o'shani o'rgataman bilganimni senga bilmaganimni sen menga o'rgatasan Dasturlashni birgalikda o'rganamiz") 
+else:
+    print(f"To'g'ri boshqa nma ham derdng barcha xatolarim uchun uzr")
+b = input("Sevgi- hozir bizzi yosh uchun umuman toʻgʻri kelmaydigan his tuygʻu menimcha yoki men qanday sevishni bilmadm yoki bila turib qadriga yetmadm  ")   
